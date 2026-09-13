02107f98029e56d3e5318a753189dc17b2c5fb1d
islam0122 <duishobaevislam01@gmail.com>
2026-09-13 16:51:30 +0600

    17-commit

--- backend/deployment/server-entrypoint.sh
+++ backend/deployment/server-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 
 exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
